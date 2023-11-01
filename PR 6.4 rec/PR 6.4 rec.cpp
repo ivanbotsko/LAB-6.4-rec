425491ecdcbf5ce016712b04661cb4718f1d1fa9
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-01 22:15:01 +0200

    first master commit

---
+++ PR 6.4 rec/PR 6.4 rec.cpp
@@ -0,0 +1,112 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+std::vector<int> generateArray(int size, int min, int max) {
+    std::vector<int> array;
+    for (int i = 0; i < size; i++) {
+        array.push_back(rand() % (max - min + 1) + min);
+    }
+    return array;
+}
+
+int calcSumOfOdd(const std::vector<int>& array) {
+    int sum = 0;
+    for (int i = 0; i < array.size(); i++) {
+        if (i % 2 != 0) {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int calcFirstNegative(const std::vector<int>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        if (array[i] < 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int calcLastNegative(const std::vector<int>& array) {
+    int lastNegativeIndex = -1;
+    for (int i = 0; i < array.size(); i++) {
+        if (array[i] < 0) {
+            lastNegativeIndex = i;
+        }
+    }
+    return lastNegativeIndex;
+}
+
+int calcSumBetweenNeg(const std::vector<int>& array, int firstNegativeIdx, int lastNegativeIdx) {
+    int sum = 0;
+    for (int i = firstNegativeIdx; i <= lastNegativeIdx; i++) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+std::vector<int> compressArray(std::vector<int>& array) {
+    int index = 0;
+    for (int i = 0; i < array.size(); i++) {
+        if (std::abs(array[i]) > 1) {
+            array[index] = array[i];
+            index++;
+        }
+    }
+
+    while (index < array.size()) {
+        array[index] = 0;
+        index++;
+    }
+    array.resize(index);
+
+    return array;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(0)));
+
+    int n;
+    std::cout << "n = ";
+    std::cin >> n;
+
+    std::vector<int> array = generateArray(n, -5, 5);
+
+    std::cout << "Initial array: ";
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << array[i];
+        if (i < array.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    int sumOfOdd = calcSumOfOdd(array);
+    std::cout << "The sum of elements with odd indices: " << sumOfOdd << std::endl;
+
+    int firstNegIdx = calcFirstNegative(array);
+    int lastNegIdx = calcLastNegative(array);
+
+    if (firstNegIdx != -1 && lastNegIdx != -1) {
+        int sumBetweenNeg = calcSumBetweenNeg(array, firstNegIdx, lastNegIdx);
+        std::cout << "The sum of the elements between the negative elements:" << sumBetweenNeg << std::endl;
+    }
+    else {
+        std::cout << "There are no negative elements in the array." << std::endl;
+    }
+
+    std::vector<int> compressedArray = compressArray(array);
+    std::cout << "Compressed array: ";
+    for (int i = 0; i < compressedArray.size(); i++) {
+        std::cout << compressedArray[i];
+        if (i < compressedArray.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,53 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "../PR 6.4 rec/PR 6.4 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(GenerateArrayTest) {
+public:
+
+    TEST_METHOD(GeneratesArrayWithCorrectSize) {
+        int size = 26;
+        int min = -20;
+        int max = 40;
+
+        std::vector<int> array = generateArray(size, min, max);
+
+        Assert::AreEqual(size, static_cast<int>(array.size()));
+    }
+
+    TEST_METHOD(GeneratesArrayWithinSpecifiedRange) {
+        int size = 20;
+        int min = -20;
+        int max = 40;
+
+        std::vector<int> array = generateArray(size, min, max);
+
+        for (int element : array) {
+            Assert::IsTrue(element >= min && element <= max);
+        }
+    }
+
+    TEST_METHOD(GeneratedArrayIsRandom) {
+        int size = 100;
+        int min = -50;
+        int max = 50;
+
+        std::vector<int> array1 = generateArray(size, min, max);
+        std::vector<int> array2 = generateArray(size, min, max);
+
+        bool areDifferent = false;
+
+        // Перевірка, чи створені масиви відмінні один від одного
+        for (int i = 0; i < size; i++) {
+            if (array1[i] != array2[i]) {
+                areDifferent = true;
+                break;
+            }
+        }
+
+        Assert::IsTrue(areDifferent);
+    }
+};
